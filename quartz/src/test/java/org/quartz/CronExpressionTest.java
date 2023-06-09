082c5bf6570b9230cf2ab64bd960e6deb909d583
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-09 08:20:42 -0400

    spelling: previous
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- quartz/src/test/java/org/quartz/CronExpressionTest.java
+++ quartz/src/test/java/org/quartz/CronExpressionTest.java
@@ -141,12 +141,12 @@ public class CronExpressionTest extends SerializationTestSupport {
  		Trigger trigger = TriggerBuilder.newTrigger().withIdentity("test").withSchedule(schedBuilder).build();
  				
  		int i = 0;
- 		Date pdate = trigger.getFireTimeAfter(new Date());
+ 		Date previousDate = trigger.getFireTimeAfter(new Date());
  		while (++i < 26) {
- 			Date date = trigger.getFireTimeAfter(pdate);
- 			System.out.println("fireTime: " + date + ", previousFireTime: " + pdate);
- 			assertFalse("Next fire time is the same as previous fire time!", pdate.equals(date));
- 			pdate = date;
+ 			Date date = trigger.getFireTimeAfter(previousDate);
+ 			System.out.println("fireTime: " + date + ", previousFireTime: " + previousDate);
+ 			assertFalse("Next fire time is the same as previous fire time!", previousDate.equals(date));
+ 			previousDate = date;
  		}
  	}
     
